769db23e0a606a9798b089ad99d0aac14bbe7946
HaloReach13 <112941224+juhaszb1@users.noreply.github.com>
2024-10-13 20:34:27 +0200

    Decode and encode done, symbols change must be solved

---
+++ MorzeCode/MorzeCodeClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class MorzeCodeClass
+{
+    public Dictionary<char, string> morzeToText;
+
+    public string shortSymbol;
+
+    public string longSymbol;
+
+    public MorzeCodeClass(string ShortSymbol, string LongSymbol)
+    {
+        this.shortSymbol = ShortSymbol;
+        this.longSymbol = LongSymbol;
+        Dictionary();
+    }
+
+    public void Dictionary()
+    {
+        string[] file = File.ReadAllLines("morze.txt");
+        morzeToText = new Dictionary<char, string>();
+        foreach (var item in file)
+        {
+            string[] split = item.Split(";");
+            morzeToText.Add(char.Parse(split[0]), split[1]);
+        }
+    }
+
+    public string Decode(string morseCode)
+    {
+        StringBuilder convertedMessage = new StringBuilder();
+        string[] letters = morseCode.Split(' ');
+        foreach (var item in letters)
+        {
+            if (morzeToText.ContainsValue(item))
+            {
+                convertedMessage.Append(morzeToText.FirstOrDefault(s => s.Value == item).Key);
+            }
+            else
+            {
+                convertedMessage.Append('?');
+            }
+        }
+        convertedMessage.Append(' ');
+        return convertedMessage.ToString().Trim();
+    }
+
+    public string Encode(string text)
+    {
+        StringBuilder convertMessage = new StringBuilder();
+        foreach (var item in text) 
+        {
+            if (morzeToText.ContainsKey(item))
+            {
+                convertMessage.Append(morzeToText[item]);
+            }
+            else
+            {
+                convertMessage.Append('?');
+            }
+        }
+        convertMessage.Append(' ');
+        return convertMessage.ToString().Trim();
+    }
+}
---
+++ MorzeCode/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Program
+{
+    public static MorzeCodeClass translator;
+    public static void SymbolInput()
+    {
+        Console.Write("Add a short symbol: ");
+        string shortSymbol = Console.ReadLine();
+        Console.Write("Add a long symbol: ");
+        string longSymbol = Console.ReadLine();
+
+        if (shortSymbol.Length == 1 && longSymbol.Length == 1 && shortSymbol != longSymbol)
+        {
+            translator = new MorzeCodeClass(shortSymbol, longSymbol);
+        }
+        else
+        {
+            throw new Exception("Wrong Input!");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        SymbolInput();
+        bool menu = true;
+        while (menu)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Morze to Text");
+            Console.WriteLine("2. Text to Morze");
+            Console.WriteLine("3. Exit");
+            string choice = Console.ReadLine();
+
+            switch (choice) 
+            {
+                case "1":
+                    Console.Write("Add a morze code: ");
+                    string morze = Console.ReadLine();
+                    string decodedMessage = translator.Decode(morze);
+                    Console.WriteLine($"Original text: {decodedMessage}");
+                    Console.ReadLine();
+                    break;
+                case "2":
+                    Console.Write("Add a text: ");
+                    string text = Console.ReadLine();
+                    string encodedMessage = translator.Encode(text.ToUpper());
+                    Console.WriteLine($"Original text: {encodedMessage}");
+                    Console.ReadLine();
+                    break;
+                case "3":
+                    Console.WriteLine("Continue? [y/n]");
+                    string cont = Console.ReadLine();
+                    if (cont == "y")
+                    {
+                        menu = false;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    break;
+            }
+        }
+    }
+}
