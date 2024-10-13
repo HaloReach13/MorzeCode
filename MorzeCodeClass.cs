2997b83754b6fabf85d646218d121db1558f5d52
Juhász Balázs <75490235+HaloReach13@users.noreply.github.com>
2024-10-13 15:56:24 +0200

    Morze Task
    I made a morze code translator console application.

---
+++ MorzeCodeClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class MorzeCodeClass
+{
+    public Dictionary<string, char> morzeToText;
+    public string dotSymbol { get; set; }
+    public string dashSymbol { get; set; }
+
+    public MorzeCodeClass()
+    {
+        dotSymbol = ".";
+        dashSymbol = "-";
+        Dictionary();
+    }
+
+    public void Dictionary()
+    {
+        morzeToText = new Dictionary<string, char>
+        {
+            { $"{dotSymbol}{dashSymbol}", 'A' },
+            { $"{dashSymbol}{dotSymbol}{dotSymbol}{dotSymbol}", 'B' },
+            { $"{dashSymbol}{dotSymbol}{dashSymbol}{dotSymbol}", 'C' },
+            { $"{dashSymbol}{dotSymbol}{dotSymbol}", 'D' },
+            { $"{dotSymbol}", 'E' },
+            { $"{dotSymbol}{dotSymbol}{dashSymbol}{dotSymbol}", 'F' },
+            { $"{dashSymbol}{dashSymbol}{dotSymbol}", 'G' },
+            { $"{dotSymbol}{dotSymbol}{dotSymbol}{dotSymbol}", 'H' },
+            { $"{dotSymbol}{dotSymbol}", 'I' },
+            { $"{dotSymbol}{dashSymbol}{dashSymbol}{dashSymbol}", 'J' },
+            { $"{dashSymbol}{dotSymbol}{dashSymbol}", 'K' },
+            { $"{dotSymbol}{dashSymbol}{dotSymbol}{dotSymbol}", 'L' },
+            { $"{dashSymbol}{dashSymbol}", 'M' },
+            { $"{dashSymbol}{dotSymbol}", 'N' },
+            { $"{dashSymbol}{dashSymbol}{dashSymbol}", 'O' },
+            { $"{dotSymbol}{dashSymbol}{dashSymbol}{dotSymbol}", 'P' },
+            { $"{dashSymbol}{dashSymbol}{dotSymbol}{dashSymbol}", 'Q' },
+            { $"{dotSymbol}{dashSymbol}{dotSymbol}", 'R' },
+            { $"{dotSymbol}{dotSymbol}{dotSymbol}", 'S' },
+            { $"{dashSymbol}", 'T' },
+            { $"{dotSymbol}{dotSymbol}{dashSymbol}", 'U' },
+            { $"{dotSymbol}{dotSymbol}{dotSymbol}{dashSymbol}", 'V' },
+            { $"{dotSymbol}{dashSymbol}{dashSymbol}", 'W' },
+            { $"{dashSymbol}{dotSymbol}{dotSymbol}{dashSymbol}", 'X' },
+            { $"{dashSymbol}{dotSymbol}{dashSymbol}{dashSymbol}", 'Y' },
+            { $"{dashSymbol}{dashSymbol}{dotSymbol}{dotSymbol}", 'Z' }
+        };
+    }
+
+    public string Convert(string morseCode)
+    {
+        StringBuilder convertedMessage = new StringBuilder();
+        string[] words = morseCode.Split(" / ");
+        foreach (var item in words)
+        {
+            string[] letters = item.Split(' ');
+            foreach (var item2 in letters)
+            {
+                if (morzeToText.ContainsKey(item2))
+                {
+                    convertedMessage.Append(morzeToText[item2]);
+                }
+                else
+                {
+                    convertedMessage.Append('?');
+                }
+            }
+            convertedMessage.Append(' ');
+        }
+        return convertedMessage.ToString().Trim();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MorzeCodeClass translator = new MorzeCodeClass();
+
+        Console.Write("Add a morze code: ");
+        string morze = Console.ReadLine();
+        string convertedMessage = translator.Convert(morze);
+        Console.WriteLine($"Original text: {convertedMessage}");
+    }
+}
